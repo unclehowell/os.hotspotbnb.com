e6a8fde6c33cefe44011e4cad670b4537e1cee6c
Wave <10119070+WaveOS-RPI3@users.noreply.github.com>
2018-02-02 16:13:21 -0300

    Update index.php

--- var/www/html/index.php
+++ var/www/html/index.php
@@ -1,8 +1,9 @@
-# location of this file /var/www/html/
+# location of this file /var/www/html/pihole/
 <?php $port = '8080';
       header('Location: '
     . ($_SERVER['HTTPS'] ? 'https' : 'http')
-    . '://' . $_SERVER['HTTP_HOST'] . ':' . $port  . '/' . 'habpanel' . '/' . '$
+    . '://' . $_SERVER['HTTP_HOST'] . ':' . $port  . '/' . 'habpanel' . '/' . 'index.html' . '#'
     . $_SERVER['REQUEST_URI']);
 exit;
 ?>
+
