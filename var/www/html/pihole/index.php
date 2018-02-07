cf955847e697cc9a86cdcad7a4d908eaeaf28c01
Hywel ApBuckler <10119070+unclehowell@users.noreply.github.com>
2018-02-07 11:21:30 -0300

    more of my time
    more of my time

--- var/www/html/pihole/index.php
+++
@@ -1,9 +0,0 @@
-# location of this file /var/www/html/pihole/
-<?php $port = '8080';
-      header('Location: '
-    . ($_SERVER['HTTPS'] ? 'https' : 'http')
-    . '://' . $_SERVER['HTTP_HOST'] . ':' . $port  . '/' . 'habpanel' . '/' . 'index.html' . '#'
-    . $_SERVER['REQUEST_URI']);
-exit;
-?>
-
