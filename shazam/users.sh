398c5f2359e5651e807ae366f2e0686ce4f1b8ec
Hywel ApBuckler <10119070+unclehowell@users.noreply.github.com>
2018-02-10 13:09:43 -0300

    Create users.sh

---
+++ shazam/users.sh
@@ -0,0 +1,16 @@
+ # default user
+----------------------------------------------------------------------------------------------------------------------------------
+                         :   Old Defaults (v0.1.3)     :::::::                       New Defaults (v0.1.4)                   
+----------------------------------------------------------------------------------------------------------------------------------
+   App/Service           :   USERNAME :    PASSWORD    :::::::    ADMIN ID :   ADMIN KEY    :: GUEST ID  :  GUEST KEY    :
+  --------------------------------------------------------------------------------------------------------------------------------
+      Wi-Fi              :    N/A     :   makeitwave   :::::::     N/A     : (Processor ID) ::    N/A    :  makeitwave   :
+      SSH                :    dietpi  :     dietpi     :::::::     wave    : (Processor ID) ::    wave   :  makeitwave   :
+      EMBY               :            :                :::::::     wave    : (Processor ID) ::    wave   :  makeitwave   :
+     EmonCMS             :            :                :::::::     wave    : (Processor ID) ::    wave   :  makeitwave   :      
+    MotionEye            :            :                :::::::     wave    : (Processor ID) ::    wave   :  makeitwave   :          
+     RaspAP              :            :                :::::::     wave    : (Processor ID) ::    wave   :  makeitwave   :            
+    TightVNC             :            :                :::::::     wave    : (Processor ID) ::    wave   :  makeitwave   :          
+    Guacamole            :   dietpi   :  Burgerking!!  :::::::     wave    : (Processor ID) ::    wave   :  makeitwave   : 
+
+
