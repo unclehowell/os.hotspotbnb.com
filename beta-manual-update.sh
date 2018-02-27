ab317f0fce1f0fcc94376b9cdbf40e9c6b684723
Hywel ApBuckler <10119070+unclehowell@users.noreply.github.com>
2018-02-27 20:41:02 -0300

    Delete beta-manual-update.sh

--- beta-manual-update.sh
+++
@@ -1,1 +0,0 @@
-rm -r /opt/WaveOS && git clone -b slave --single-branch https://github.com/unclehowell/WaveOS.git /opt/WaveOS && chmod +x /opt/WaveOS/dietpi-to-wave.sh && sh /opt/WaveOS/dietpi-to-wave.sh
