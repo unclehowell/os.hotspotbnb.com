74ba363417f22f8b9487789c21178a9a6417b3d7
Hywel ApBuckler <10119070+unclehowell@users.noreply.github.com>
2018-02-27 20:34:47 -0300

    Create beta-manual-update.sh

---
+++ beta-manual-update.sh
@@ -0,0 +1,1 @@
+rm -r /opt/WaveOS && git clone -b slave --single-branch https://github.com/unclehowell/WaveOS.git /opt/WaveOS && chmod +x /opt/WaveOS/dietpi-to-wave.sh && sh /opt/WaveOS/dietpi-to-wave.sh
