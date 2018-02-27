cf0db0fa3a03112814993ec7e836b4dae904e38e
Hywel ApBuckler <10119070+unclehowell@users.noreply.github.com>
2018-02-27 20:07:25 -0300

    Create install-dietpi-apps.sh

---
+++ install-dietpi-apps.sh
@@ -0,0 +1,26 @@
+# We need to do this as a run once. We can't possibly re-install all the programms each time an update happens.
+# In most cases the system should see the programs installed and skip, but the workarounds we use here might cause full re-installs on update.
+# Let's hash it out and see
+
+# First we need this program which apparently simulates keystrokes. We're basically going to trick PiHole into thinking a human is entering the settings on its menu.
+
+apt-get install xdotool -y
+
+# now for pihole ,using this guide https://blog.sleeplessbeastie.eu/2018/01/11/how-to-install-and-configure-pi-hole/
+
+apt-get install git net-tools -y
+
+git clone --depth 1 https://github.com/unclehowell/pi-hole.git pi-hole
+
+# Then we use our custom basic-install script, which has the xdottool commands in it (to simulate a human configuring it). Otherwise it will cease installing while it waits for a user to enter the settings
+
+sudo bash pi-hole/automated\ install/fully-automated-install.sh
+
+
+
+
+
+
+
+
+
