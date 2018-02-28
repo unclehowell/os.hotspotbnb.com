bd1f13cebae5376874facbe8c0019fe2faa89c58
Hywel ApBuckler <10119070+unclehowell@users.noreply.github.com>
2018-02-28 03:52:15 -0300

    Update install-dietpi-apps.sh

--- install-dietpi-apps.sh
+++ install-dietpi-apps.sh
@@ -35,7 +35,8 @@ chmod +x /opt/WaveOS/hit-enter.sh
 
 # install pihole
 
-curl -sSL https://install.pi-hole.net & sleep 50 python /opt/WaveOS/hit-enter.py
+curl -sSL https://install.pi-hole.net
+sleep 50 python /opt/WaveOS/hit-enter.py
 
 
 
