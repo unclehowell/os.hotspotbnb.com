8bdd0deaa9de8247d801be73404d7600343707dc
Hywel ApBuckler <10119070+unclehowell@users.noreply.github.com>
2018-02-28 02:05:52 -0300

    Update install-dietpi-apps.sh

--- install-dietpi-apps.sh
+++ install-dietpi-apps.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 # We need to do this as a run once. We can't possibly re-install all the programms each time an update happens.
 # In most cases the system should see the programs installed and skip, but the workarounds we use here might cause full re-installs on update.
 # Let's hash it out and see
@@ -25,6 +27,18 @@ cd /root/python-uinput/
 python setup.py build
 python setup.py install
 chmod +x /opt/WaveOS/hit-enter.py
+
+# install pihole
+curl -sSL https://install.pi-hole.net | bash  
+
+typeset -F SECONDS=0; repeat 10 {date; ((n=5-SECONDS)); SECONDS=0;sleep $n}
+python /opt/WaveOS/hit-enter.py
+
+typeset -F SECONDS=0; repeat 10 {date; ((n=5-SECONDS)); SECONDS=0;sleep $n}
+python /opt/WaveOS/hit-enter.py
+
+typeset -F SECONDS=0; repeat 10 {date; ((n=5-SECONDS)); SECONDS=0;sleep $n}
+python /opt/WaveOS/hit-enter.py
 
 
 
