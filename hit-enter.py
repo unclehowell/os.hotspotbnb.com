83678fd61eb38285c79fe013be33a2916cdd639e
Hywel ApBuckler <10119070+unclehowell@users.noreply.github.com>
2018-02-28 02:26:55 -0300

    Update hit-enter.py

--- hit-enter.py
+++ hit-enter.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python
 
+def AltTab():
+    """Press Alt+Tab and hold Alt key for 2 seconds
+    in order to see the overlay.
+    """
+    PressKey(VK_MENU)   # Alt
+    PressKey(VK_TAB)    # Tab
+    ReleaseKey(VK_TAB)  # Tab~
+    time.sleep(2)
+    ReleaseKey(VK_MENU) # Alt~
+
+
 import uinput
 
 device = uinput.Device([
