b8b59a7ce331072dfff5ed9eaa99f0dbdae89e49
Hywel ApBuckler <10119070+unclehowell@users.noreply.github.com>
2018-02-28 00:03:37 -0300

    Update install-dietpi-apps.sh

--- install-dietpi-apps.sh
+++ install-dietpi-apps.sh
@@ -1,36 +1,18 @@
 # We need to do this as a run once. We can't possibly re-install all the programms each time an update happens.
 # In most cases the system should see the programs installed and skip, but the workarounds we use here might cause full re-installs on update.
 # Let's hash it out and see
-
 apt-get autoremove -y
 
-# previous install of pihole, conflicts with the auto installer (may need to remove both these lines as the files appear to be ghost files of previous work)
-rm /root/pi-hole/automated\ install/auto-install.sh
-rm /root/pi-hole/gravity.sh
-
-# netdata
-
-apt-get install zlib1g-dev uuid-dev libmnl-dev gcc make git autoconf autoconf-archive autogen automake pkg-config curl -y
-git clone https://github.com/unclehowell/WaveOS_App--netdata.git --depth=1
-cd netdata
-./netdata-installer.sh --install /opt
-
 # now for pihole ,using this guide https://blog.sleeplessbeastie.eu/2018/01/11/how-to-install-and-configure-pi-hole/
-
 apt-get install git net-tools -y
-
 git clone --depth 1 https://github.com/unclehowell/pi-hole.git pi-hole
 
-# Then we run the autonomous version of the script, to save us going through menu options
+# before we run the install we introduce the automoated install script
+git clone --depth 1 https://github.com/unclehowell/WaveOS-Core--automate-PiHole.git pi-hole-automate
+sudo bash root/pi-hole-automate/UninstallOriginal.sh
+sudo bash root/pi-hole-automate/autoinstallTest.sh
 
-rm /root/pi-hole/automated\ install/auto-install.sh
-
-mv /etc/.pihole/gravity.sh /root/pi-hole/automated\ install/
-cp /root/pi-hole/automated\ install/gravity.sh /etc/.pihole/
-
-wget https://raw.githubusercontent.com/unclehowell/WaveOS-Core--automate-PiHole/master/auto-install.sh -P /root/pi-hole/automated\ install/
-
-sudo bash /root/pi-hole/automated\ install/auto-install.sh
+# plan b - copy the auto script into the pi-hole repo and run it from there /root/pi-hole/automated\ install/auto-install.sh
 
 
 
