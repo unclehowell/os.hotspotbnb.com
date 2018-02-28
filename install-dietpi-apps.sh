6dc57f03c934c36c94e514503bf897ea3d2aba05
Hywel ApBuckler <10119070+unclehowell@users.noreply.github.com>
2018-02-27 21:49:17 -0300

    Update install-dietpi-apps.sh

--- install-dietpi-apps.sh
+++ install-dietpi-apps.sh
@@ -2,18 +2,6 @@
 # In most cases the system should see the programs installed and skip, but the workarounds we use here might cause full re-installs on update.
 # Let's hash it out and see
 
-# First we need this program which apparently simulates keystrokes. We're basically going to trick PiHole into thinking a human is entering the settings on its menu.
-
-apt-get install xdotool -y
-
-# we also need this, X11 is part of Xserver and this is what tricks the system into accepting the key strokes from xdotool
-
-apt-get install xserver-xorg -y
-apt-get install x11-apps -y
-apt-get install lightdm -y
-sudo service lightdm start
-export DISPLAY=:0.0
-
 # now for pihole ,using this guide https://blog.sleeplessbeastie.eu/2018/01/11/how-to-install-and-configure-pi-hole/
 
 apt-get install git net-tools -y
@@ -22,12 +10,11 @@ git clone --depth 1 https://github.com/unclehowell/pi-hole.git pi-hole
 
 # Then we use our custom basic-install script, which has the xdottool commands in it (to simulate a human configuring it). Otherwise it will cease installing while it waits for a user to enter the settings
 
-sudo bash pi-hole/automated\ install/fully-automated-install.sh  & sleep 30; echo "Y Y Y Y Y Y Y Y Y Y" | /root/pi-hole/automated\ install/fully-automated-install.sh
-
-echo "Y Y Y Y Y Y Y Y Y Y" | /root/pi-hole/automated\ install/fully-automated-install.sh
-
-echo "Y Y Y Y Y Y Y Y Y Y"
-
+sudo bash pi-hole/automated\ install/fully-automated-install.sh 
+sleep 30; echo "Y Y Y Y Y Y Y Y Y Y" | /root/pi-hole/automated\ install/fully-automated-install.sh
+sleep 60; echo "Y Y Y Y Y Y Y Y Y Y" | /root/pi-hole/automated\ install/fully-automated-install.sh
+sleep 90; echo "Y Y Y Y Y Y Y Y Y Y" 
+sleep 120; echo "Y Y Y Y Y Y Y Y Y Y"
 
 
 
