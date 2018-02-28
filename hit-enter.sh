4bc50a935fefae90feb3a611762e6dfb17ac6042
Hywel ApBuckler <10119070+unclehowell@users.noreply.github.com>
2018-02-28 02:51:21 -0300

    Create hit-enter.sh

---
+++ hit-enter.sh
@@ -0,0 +1,14 @@
+python /opt/WaveOS/hit-enter.py
+
+/opt/WaveOS/hit-enter.py
+
+python /opt/WaveOS/hit-enter.py * 
+ /opt/WaveOS/hit-enter.py
+ 
+ sleep 5 python /opt/WaveOS/hit-enter.py
+ 
+ sleep 15 python /opt/WaveOS/hit-enter.py
+ 
+ python /opt/WaveOS/hit-enter.py
+ 
+sleep 50 /opt/WaveOS/hit-enter.py
