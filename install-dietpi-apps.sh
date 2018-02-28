ac8792fb033cc49be777dd341f51b65a1c79fe61
Hywel ApBuckler <10119070+unclehowell@users.noreply.github.com>
2018-02-28 00:24:03 -0300

    Update install-dietpi-apps.sh

--- install-dietpi-apps.sh
+++ install-dietpi-apps.sh
@@ -3,14 +3,18 @@
 # Let's hash it out and see
 apt-get autoremove -y
 
+# TESTING PURPOSES - remove and redo each time
+rm -r /root/pi-hole-automate
+rm -r /root/pi-hole/automated\ install
+
 # now for pihole ,using this guide https://blog.sleeplessbeastie.eu/2018/01/11/how-to-install-and-configure-pi-hole/
 apt-get install git net-tools -y
 git clone --depth 1 https://github.com/unclehowell/pi-hole.git pi-hole
 
-# before we run the install we introduce the automoated install script
+# before we run the install we introduce the automoated install script and run that instead
 git clone --depth 1 https://github.com/unclehowell/WaveOS-Core--automate-PiHole.git pi-hole-automate
-sudo bash root/pi-hole-automate/UninstallOriginal.sh
-sudo bash root/pi-hole-automate/autoinstallTest.sh
+sudo bash /root/pi-hole-automate/UninstallOriginal.sh
+sudo bash /root/pi-hole-automate/autoinstallTest.sh
 
 # plan b - copy the auto script into the pi-hole repo and run it from there /root/pi-hole/automated\ install/auto-install.sh
 
