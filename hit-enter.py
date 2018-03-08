2ed5844db1c6edb38a5b78f314215a98f998e483
Hywel ApBuckler <10119070+unclehowell@users.noreply.github.com>
2018-03-08 12:52:55 -0300

    Delete hit-enter.py

--- hit-enter.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-from evdev import uinput, ecodes as e
-
-with uinput.UInput() as ui:
-     ui.write(e.EV_KEY, e.KEY_ENTER, 1)
-     ui.write(e.EV_KEY, e.KEY_ENTER, 0)
-     ui.syn()
