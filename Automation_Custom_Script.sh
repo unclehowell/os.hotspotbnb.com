b22cb65bf97463b069223fdfe6c47a80770fbbab
Hywel ApBuckler <10119070+unclehowell@users.noreply.github.com>
2018-03-30 19:38:28 -0300

    Update Automation_Custom_Script.sh

--- Automation_Custom_Script.sh
+++ Automation_Custom_Script.sh
@@ -1,1 +1,12 @@
 
+apt-get install fbi -y
+
+
+cp http://www.stickpng.com/assets/images/5866133d7d90850fc3ce2a54.png /etc/
+
+touch asplashscreen 
+
+mv asplashscreen /etc/init.d/
+
+sudo nano /etc/init.d/asplashscreen
+
