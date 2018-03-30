e58e08ed8d2853f5ee4ecade87ba4710c3abbfd7
Hywel ApBuckler <10119070+unclehowell@users.noreply.github.com>
2018-03-30 19:50:38 -0300

    Update Automation_Custom_Script.sh

--- Automation_Custom_Script.sh
+++ Automation_Custom_Script.sh
@@ -8,7 +8,8 @@ mv /etc/5866133d7d90850fc3ce2a54.png splash.png
 
 cp  /etc/init.d/
 
-git pull
+git pull https://github.com/unclehowell/WaveOS/blob/master/splashscreen.sh /etc/init.d/asplashscreen
+
 chmod a+x /etc/init.d/asplashscreen
 
 insserv /etc/init.d/asplashscreen
