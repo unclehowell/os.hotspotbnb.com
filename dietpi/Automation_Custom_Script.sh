6732a6408c692e1122996248476c1fd52638758e
Hywel ApBuckler <10119070+unclehowell@users.noreply.github.com>
2018-03-31 03:12:00 -0300

    Update Automation_Custom_Script.sh

--- dietpi/Automation_Custom_Script.sh
+++ dietpi/Automation_Custom_Script.sh
@@ -1,1 +1,20 @@
 sudo apt-get autoremove -y
+
+apt-get install fbi -y
+
+
+cp http://www.stickpng.com/assets/images/5866133d7d90850fc3ce2a54.png /etc/
+
+mv /etc/5866133d7d90850fc3ce2a54.png splash.png
+
+cp  /etc/init.d/
+
+git fetch https://github.com/unclehowell/WaveOS/blob/master/splashscreen.sh /etc/init.d/
+
+mv /etc/init.d/splashscreen.sh /etc/init.d/splashscreen
+
+chmod a+x /etc/init.d/asplashscreen
+
+insserv /etc/init.d/asplashscreen
+
+reboot
