0b185147fe03f7b7bf282f814e7e8f463ec5f348
Hywel ApBuckler <10119070+unclehowell@users.noreply.github.com>
2018-04-24 20:42:27 -0300

    Update cleanup.sh

--- server/cross-device-scripts/features/cleanup.sh
+++ server/cross-device-scripts/features/cleanup.sh
@@ -1,6 +1,10 @@
 # this file can be reference throughout the install, it's just some basic cleanup commands
 rm -r /var/WaveOS
 
+cd /var
+git clone https://github.com/unclehowell/WaveOS.git
+chmod +x /var/WaveOS
+
 rm -r /var/www/js
 rm -r /var/www/css
 
