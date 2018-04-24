0924605ace108cd021b98b12cad6c1c6d57115f5
Hywel ApBuckler <10119070+unclehowell@users.noreply.github.com>
2018-04-24 15:02:13 -0300

    Update boot-up.sh

--- server/cross-device-scripts/boot-up.sh
+++ server/cross-device-scripts/boot-up.sh
@@ -6,18 +6,4 @@
 
 # Mode-Menu Splash Page (still working on this)
 
-sudo git clone https://github.com/unclehowell/WaveOS.git var
-
-sudo rm -r /var/www/index.lighttpd.html
-
-cd /var/WaveOS/server/cross-device-scripts/features/multistep-menu/
-
-mv index.html var/www
-
-mv css/* var/www
-
-mv js/* var/www
-
-cd
-
 
