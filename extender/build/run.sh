e565dba344750e0ad528b33e9f61baeb6db8c881
Hywel ApBuckler <10119070+unclehowell@users.noreply.github.com>
2018-04-24 13:33:13 -0300

    Update run.sh

--- extender/build/run.sh
+++ extender/build/run.sh
@@ -5,7 +5,11 @@
 sudo dietpi-software install 29
 
 # chromium
+sudo apt-get update
+sudo apt-get dist-upgrade
 sudo apt-get install --no-install-recommends chromium-browser
+sudo apt-get install -y rpi-chromium-mods
+sudo apt-get install -y python-sense-emu python3-sense-emu
 
 # X-server (Linux Display System)
 sudo dietpi-software install 6
@@ -13,13 +17,13 @@ sudo dietpi-software install 6
 # Chromium Kiosk Mode  (guide here https://die-antwort.eu/techblog/2017-12-setup-raspberry-pi-for-kiosk-mode/)
 
 sudo apt-get update && apt-get upgrade -y
+sudo apt-get install chromium x11-xserver-utils unclutter chromium-bsu
 sudo apt-get install --no-install-recommends xserver-xorg xinit openbox
-sudo apt-get install chromium x11-xserver-utils unclutter
 sed -i -e 's/screen saver/#screen saver/g' /etc/xdg/lxsession/LXDE/autostart
 echo -n "@xset s off" >> </etc/xdg/lxsession/LXDE/autostart>
 echo -n "@xset -dpms" >> </etc/xdg/lxsession/LXDE/autostart>
 echo -n "@xset s noblank" >> </etc/xdg/lxsession/LXDE/autostart>
-echo -n "@chromium --kiosk --incognito http://wave/" >> </etc/xdg/lxsession/LXDE/autostart>
+echo -n "@chromium --kiosk --incognito http://10.0.0.10" >> </etc/xdg/lxsession/LXDE/autostart>
 
 # Docker (for Guacamole)
 sudo dietpi-software install 162
