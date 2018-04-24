ec5e628d503ebccf67afbdd33e2e47b3512ac09d
Hywel ApBuckler <10119070+unclehowell@users.noreply.github.com>
2018-04-24 20:28:15 -0300

    Update go.sh

--- server/cross-device-scripts/features/broadcast-hostname/go.sh
+++ server/cross-device-scripts/features/broadcast-hostname/go.sh
@@ -2,12 +2,15 @@
 
 /DietPi/dietpi/dietpi-software install 152 
 
+# tried this - didn't seem to work
 apt-get install insserv
-
 insserv avahi-daemon
-
 /etc/init.d/avahi-daemon restart
 
+# tried this - didn't seem to work
+
+mv /var/WaveOS/server/cross-device-scripts/features/broadcast-hostname/dnsmasq.txt /etc/default/
+mv /etc/default/dnsmasq.txt /etc/default/dnsmasq
 
 
 
