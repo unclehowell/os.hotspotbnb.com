d6aef2e38a5ac0bf06480e862d72fcf62ef0aef8
Hywel ApBuckler <10119070+unclehowell@users.noreply.github.com>
2018-04-24 20:08:46 -0300

    Update broadcast-hostname.sh

--- server/cross-device-scripts/features/broadcast-hostname.sh
+++ server/cross-device-scripts/features/broadcast-hostname.sh
@@ -1,4 +1,8 @@
 # will install Avahi-Daemon: to connect to the unit without needing ip addresses
 
-/DietPi/dietpi/dietpi-software install 152  
+/DietPi/dietpi/dietpi-software install 152 
+
+sudo apt-get install insserv
+
+sudo insserv avahi-daemon
 
