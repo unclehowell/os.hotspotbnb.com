7e17afdc5d620d5f385130e17848c608f4365f6e
Hywel ApBuckler <10119070+unclehowell@users.noreply.github.com>
2018-04-24 14:39:33 -0300

    Create hosting.php

---
+++ server/cross-device-scripts/features/hosting.php
@@ -0,0 +1,5 @@
+# autonomous installation of hosting dependancies, apps and config
+
+
+# will install PHP, Lighttpd & sql-light: for hosting (the mode-menu splash-page) 
+dietpi-software install=81  
