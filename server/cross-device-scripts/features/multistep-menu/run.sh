698d8078107f80ad47df1ed33643fce8f3bb0dbc
Hywel ApBuckler <10119070+unclehowell@users.noreply.github.com>
2018-04-24 19:45:12 -0300

    Update run.sh

--- server/cross-device-scripts/features/multistep-menu/run.sh
+++ server/cross-device-scripts/features/multistep-menu/run.sh
@@ -1,15 +1,13 @@
 sudo rm -r /var/www/index.lighttpd.html
 
-cd /var/WaveOS/server/cross-device-scripts/features/multistep-menu/
-
-mv index.html /var/www
+mv /var/WaveOS/server/cross-device-scripts/features/multistep-menu/index.html /var/www
 
 mkdir /var/www/js
 
 mkdir /var/www/css
 
-mv css/style.css /var/www/css
+mv /var/WaveOS/server/cross-device-scripts/features/multistep-menu/css/style.css /var/www/css
 
-mv js/index.js /var/www/js
+mv /var/WaveOS/server/cross-device-scripts/features/multistep-menu/js/index.js /var/www/js
 
 cd
