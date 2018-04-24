bb115195390900b925c4c42fc4c32f0c264f56ff
Hywel ApBuckler <10119070+unclehowell@users.noreply.github.com>
2018-04-24 15:47:44 -0300

    Update self-build-the-mode-selector.sh

--- server/cross-device-scripts/features/self-build-the-mode-selector.sh
+++ server/cross-device-scripts/features/self-build-the-mode-selector.sh
@@ -1,4 +1,6 @@
 
 # The Hotspot will already been set-up using dietpi.txt (ssid: wave-hotspot, password: makeitwave)
+# point to note: HOSTNAME=DietPi is left as is in dietpi.txt - because if we use wave it could cause conflic if there's already a hotspot set-up on the network with the hostname Wave, while an extender is being configured.
+
 
 
