030be72edb73fe6a755a62a7ddc49849d3090658
Hywel ApBuckler <10119070+unclehowell@users.noreply.github.com>
2018-04-23 23:42:12 -0400

    Delete index.php

--- server/cross-device-scripts/index.php
+++
@@ -1,7 +0,0 @@
-<?php $port = '80';
-      header('Location: '
-    . ($_SERVER['HTTPS'] ? 'https' : 'http')
-    . '://' . $_SERVER['HTTP_HOST'] . ':' . $port  . '/' . 'index.html' . '#'
-    . $_SERVER['REQUEST_URI']);
-exit;
-?>
