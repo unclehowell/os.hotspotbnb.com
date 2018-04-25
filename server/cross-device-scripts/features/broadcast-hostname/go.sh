c6d8c74a05f394c4ebca9248e7ee3008ea0d4e92
Hywel ApBuckler <10119070+unclehowell@users.noreply.github.com>
2018-04-25 14:09:09 -0300

    Update go.sh

--- server/cross-device-scripts/features/broadcast-hostname/go.sh
+++ server/cross-device-scripts/features/broadcast-hostname/go.sh
@@ -1,3 +1,12 @@
+# searched proof of concept system for all instances of hostname to find clues how I made it broadcast (using this command: grep -rnw '/' -e 'casavr')
+# here's the results - maybe it'll be of use
+# Binary file /etc/dhcp/.dhcpd.conf.swp matches - NOT INTERESTING
+# /etc/hostname:1:casavr - OF COURSE!
+# /etc/hosts:2:127.0.1.1 casavr - OF COURSE!
+# /etc/pihole/local.list:1:10.0.0.10 casavr - NOT ARRIVED AT PIHOLE PHASE YET!
+# /etc/hostapd/hostapd.conf:3:ssid=casavr  - NOT INTERESTING
+# no hidden gems there. 
+
 # will install Avahi-Daemon: to connect to the unit without needing ip addresses
 
 /DietPi/dietpi/dietpi-software install 152 
@@ -18,6 +27,28 @@ sed 's/ range/# range/' dhcpd.conf
 sed 's/ option/# option/' dhcpd.conf
 sed 's/}/# }/' dhcpd.conf
 sed 's/8.8.8.8, 8.8.4.4/192.168.142.1, 10.0.0.1/' dhcpd.conf
+
+# tried this, no ping on hostname yet
+
+sudo apt-get install samba -y
+/DietPi/dietpi/dietpi-software install 96 # samba
+/DietPi/dietpi/dietpi-software install 1  # samba client
+
+# tried replacing this, as stated in this guide : https://rasspberrypi.wordpress.com/2012/09/08/connect-to-rasspberry-pi-using-hostname/ 
+# the default in the guide is supposedly "hosts: files mdns4_minimal [NOTFOUND=return] dns mdns4". Whereas mine is "hosts:          files dns"
+# so i'll change 'files dns' to 'mdns4_minimal [NOTFOUND=return] dns mdns4' to what it suggests in the guide. 
+# sed 's/hosts:          files dns/hosts:          files mdns4_minimal [NOTFOUND=return] dns mdns4 wins/' /etc/nsswitch.conf
+# sed command didn't work, manually change (& reboot) didn't work
+# will just replace entire file with the one from the proof of concept system (v1.2) instead, see if that works.
+# windbinds not installed on the proof of concept system, but I'll try it anyways now, for added assurance/ proocess of elimination: 
+# 
+
+
+# I remember something about dhcpcd.conf (not to be confused with dhcpd.conf). 
+# This file exists on the proof of concept system (v1.2) but not on this build, so that could be the culprit.
+
+
+
 
 
 
