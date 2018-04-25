000947e6dc07cd9009bf309f73fd93e47b3e0f41
Hywel ApBuckler <10119070+unclehowell@users.noreply.github.com>
2018-04-25 19:43:01 -0300

    Update run.sh

--- server/cross-device-scripts/features/multistep-menu/run.sh
+++ server/cross-device-scripts/features/multistep-menu/run.sh
@@ -1,14 +1,27 @@
-touch /var/www/index.lighttpd.html
 sudo rm -r /var/www/index.lighttpd.html
 
 mv /var/WaveOS/server/cross-device-scripts/features/multistep-menu/index.html /var/www
 
 mkdir /var/www/js
 
+mv /var/WaveOS/server/cross-device-scripts/features/multistep-menu/js/modernizr.custom.js /var/www/js
+mv /var/WaveOS/server/cross-device-scripts/features/multistep-menu/js/grid.js /var/www/js
+
 mkdir /var/www/css
 
-mv /var/WaveOS/server/cross-device-scripts/features/multistep-menu/css/style.css /var/www/css
+mv /var/WaveOS/server/cross-device-scripts/features/multistep-menu/css/component.css /var/www/css
+mv /var/WaveOS/server/cross-device-scripts/features/multistep-menu/css/default.css /var/www/css
 
-mv /var/WaveOS/server/cross-device-scripts/features/multistep-menu/js/index.js /var/www/js
+mkdir /var/www/images
+
+mv /var/WaveOS/server/cross-device-scripts/features/multistep-menu/images/1.jpg /var/www/images
+mv /var/WaveOS/server/cross-device-scripts/features/multistep-menu/images/2.jpg /var/www/images
+mv /var/WaveOS/server/cross-device-scripts/features/multistep-menu/images/3.jpg /var/www/images
+
+mkdir /var/www/images/thumbs
+
+mv /var/WaveOS/server/cross-device-scripts/features/multistep-menu/images/thumbs/1.jpg /var/www/images/thumbs
+mv /var/WaveOS/server/cross-device-scripts/features/multistep-menu/images/thumbs/2.jpg /var/www/images/thumbs
+mv /var/WaveOS/server/cross-device-scripts/features/multistep-menu/images/thumbs/3.jpg /var/www/images/thumbs
 
 cd
