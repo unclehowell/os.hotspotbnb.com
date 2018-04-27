fb8e97d5effb5fde52fecc60f55cb605fee0a771
Hywel ApBuckler <10119070+unclehowell@users.noreply.github.com>
2018-04-27 18:07:33 -0300

    Update run.sh

--- server/cross-device-scripts/features/iptables/run.sh
+++ server/cross-device-scripts/features/iptables/run.sh
@@ -1,6 +1,6 @@
 # the proof of concept systems IP table are clear. It's worth copying these over. 
 
-iptables-restore < /var/WaveOS/server/cross-device-scripts/features/iptables/iptables iptables
+iptables-restore < /var/WaveOS/server/cross-device-scripts/features/iptables/iptables
 
 
 
