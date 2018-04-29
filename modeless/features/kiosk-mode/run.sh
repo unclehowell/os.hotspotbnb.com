7c13cb0bd8df9f192b533617e7cf54b3414ad8b0
unclehowell <hywelapbuckler@gmail.com>
2018-04-28 23:18:53 -0300

    more

--- modeless/features/kiosk-mode/run.sh
+++ modeless/features/kiosk-mode/run.sh
@@ -1,5 +1,8 @@
 # giving this whirl https://github.com/pluggemi/roon-web-controller/wiki/Diet-Pi-Installation
 
+
+###############  Roon Web Controller Server ###############
+
 sudo su
 
 cd ~
@@ -33,6 +36,51 @@ systemctl enable roon-web-controller.service
 systemctl start roon-web-controller.service
 
 # Enable extension and log watching hints
+# n/a 
+
+su dietpi
+
+cd ~
+
+cd roon-web-controller
+
+sudo systemctl stop roon-web-controller.service
+
+git pull
+
+npm install
+
+npm update
+
+sudo systemctl start roon-web-controller.service
+
+###############  Roon Web Controller Client ###############
+
+su dietpi
+
+cd ~
+
+nano .xsession
+
+mv /var/WaveOS/modeless/features/kiosk-mode/.xsession /home/dietpi
+
+# System configuration
+
+sudo su
+
+sudo apt install xserver-xorg-video-fbdev xserver-xorg-input-libinput nodm fonts-noto openbox chromium-browser -y
+
+
+apt-get install sed -y
+
+cd /etc/default/
+
+sed 's/NODM_USER=root/NODM_USER=dietpi/' nodm
+
+
+
+
+
 
 
 
