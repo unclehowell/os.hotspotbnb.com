134d7751849592baa3539dde09bea55ffb2ea6eb
unclehowell <hywelapbuckler@gmail.com>
2018-04-29 03:21:53 -0300

    removed uneeded file

--- modeless/boot-up.sh
+++ modeless/boot-up.sh
@@ -25,11 +25,29 @@ mv /var/WaveOS /var/WaveOS-old
 cd /var
 git clone https://github.com/unclehowell/WaveOS.git
 chmod +x /var/WaveOS
+chmod +x /var/WaveOS/modeless/features/add-features.sh
+chmod +x /var/WaveOS/modeless/features/cleanup.sh
 
 # Self-Build Device to the stage of Mode Selector e.g. broadcasts ssid, hostname, set-up hosting & host the mode-selector menu
 
 sh /var/WaveOS/modeless/features/cleanup.sh
-sh /var/WaveOS/modeless/features/add-features.sh
+
+# 1) Hosting: The Pi will need to have hosting capabilities so that the menu can be hosted
+
+sh /var/WaveOS/modeless/features/hosting.sh
+
+# 2) Then the menu needs to be placed into the www/ directory so it appears in the browser when the hostname is entered
+
+sh /var/WaveOS/modeless/features/multistep-menu/run.sh
+
+# 3) Then we need to get the menu up on the screen to make it even more obvious that a mode needs to be selected from the menu
+# instead of trying to get guacamole working (so the users can control what's on the screen), the menu on the screen just needs to instruct hostname access via a mobile etc
+
+sh /var/WaveOS/modeless/features/kiosk-mode/run.sh
+
+
+# The IP will need to change to avoid conflic with a 2nd, 3rd device which may connect to the network. 
+# For this to happen, we may need to scan the network, see which IP's are available and allocate one as as fixed-IP. Solution Needed Here!
 
 
 
