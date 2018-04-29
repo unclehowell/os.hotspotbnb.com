da6d76fdbe53f547ba386367de2903d521f2285f
unclehowell <hywelapbuckler@gmail.com>
2018-04-29 00:17:16 -0300

    timesync issue resolved

--- modeless/boot-up.sh
+++ modeless/boot-up.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 # Author: Hywel ApBuckler
 
+# security certificates failed in past with web-browser and updates during initial boot-up because of time-sync issues e.g. time was wrong.
+# this should be sorted within the dietpi.txt file, but just in case this is worth getting right early as possible. 
+# also, dietpi talk about making it redundant. so watch this space.
+
+timedatectl set-ntp true
+
 # get script, run script: fetch the latest repo (so remaining tasks can then execute from the local repo), then remove all trace
 
 # Remove the old Repo Achieve File - if it exists & make a new one
