f733a1e1bd30c0d7a045ca0d3f65da771f43e894
Hywel ApBuckler <10119070+unclehowell@users.noreply.github.com>
2018-05-02 18:26:45 -0300

    Update run.sh

--- modeless/features/multistep-menu/run.sh
+++ modeless/features/multistep-menu/run.sh
@@ -1,27 +1,27 @@
-sudo rm -r /var/www/index.lighttpd.html
+sudo rm -r /var/www/html/index.html
 
-mv /var/WaveOS/modeless/features/multistep-menu/index.html /var/www
+cp /var/WaveOS/modeless/features/multistep-menu/index.html /var/www/html
 
-mkdir /var/www/js
+mkdir /var/www/html/js
 
-mv /var/WaveOS/modeless/features/multistep-menu/js/modernizr.custom.js /var/www/js
-mv /var/WaveOS/modeless/features/multistep-menu/js/grid.js /var/www/js
+cp /var/WaveOS/modeless/features/multistep-menu/js/modernizr.custom.js /var/www/html/js
+cp /var/WaveOS/modeless/features/multistep-menu/js/grid.js /var/www/html/js
 
 mkdir /var/www/css
 
-mv /var/WaveOS/modeless/features/multistep-menu/css/component.css /var/www/css
-mv /var/WaveOS/modeless/features/multistep-menu/css/default.css /var/www/css
+cp /var/WaveOS/modeless/features/multistep-menu/css/component.css /var/www/html/css
+cp /var/WaveOS/modeless/features/multistep-menu/css/default.css /var/www/html/css
 
 mkdir /var/www/images
 
-mv /var/WaveOS/modeless/features/multistep-menu/images/1.jpg /var/www/images
-mv /var/WaveOS/modeless/features/multistep-menu/images/2.jpg /var/www/images
-mv /var/WaveOS/modeless/features/multistep-menu/images/3.jpg /var/www/images
+cp /var/WaveOS/modeless/features/multistep-menu/images/1.jpg /var/www/html/images
+cp /var/WaveOS/modeless/features/multistep-menu/images/2.jpg /var/www/html/images
+cp /var/WaveOS/modeless/features/multistep-menu/images/3.jpg /var/www/html/images
 
 mkdir /var/www/images/thumbs
 
-mv /var/WaveOS/modeless/features/multistep-menu/images/thumbs/1.jpg /var/www/images/thumbs
-mv /var/WaveOS/modeless/features/multistep-menu/images/thumbs/2.jpg /var/www/images/thumbs
-mv /var/WaveOS/modeless/features/multistep-menu/images/thumbs/3.jpg /var/www/images/thumbs
+cp /var/WaveOS/modeless/features/multistep-menu/images/thumbs/1.jpg /var/www/html/images/thumbs
+cp /var/WaveOS/modeless/features/multistep-menu/images/thumbs/2.jpg /var/www/html/images/thumbs
+cp /var/WaveOS/modeless/features/multistep-menu/images/thumbs/3.jpg /var/www/html/images/thumbs
 
 cd
