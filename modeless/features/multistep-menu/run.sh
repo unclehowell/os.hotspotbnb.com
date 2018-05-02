194ec3aeaa522da0011c641aaf59b9fe8b929639
Hywel ApBuckler <10119070+unclehowell@users.noreply.github.com>
2018-05-02 18:31:14 -0300

    Update run.sh

--- modeless/features/multistep-menu/run.sh
+++ modeless/features/multistep-menu/run.sh
@@ -7,18 +7,18 @@ mkdir /var/www/html/js
 cp /var/WaveOS/modeless/features/multistep-menu/js/modernizr.custom.js /var/www/html/js
 cp /var/WaveOS/modeless/features/multistep-menu/js/grid.js /var/www/html/js
 
-mkdir /var/www/css
+mkdir /var/www/html/css
 
 cp /var/WaveOS/modeless/features/multistep-menu/css/component.css /var/www/html/css
 cp /var/WaveOS/modeless/features/multistep-menu/css/default.css /var/www/html/css
 
-mkdir /var/www/images
+mkdir /var/www/html/images
 
 cp /var/WaveOS/modeless/features/multistep-menu/images/1.jpg /var/www/html/images
 cp /var/WaveOS/modeless/features/multistep-menu/images/2.jpg /var/www/html/images
 cp /var/WaveOS/modeless/features/multistep-menu/images/3.jpg /var/www/html/images
 
-mkdir /var/www/images/thumbs
+mkdir /var/www/html/images/thumbs
 
 cp /var/WaveOS/modeless/features/multistep-menu/images/thumbs/1.jpg /var/www/html/images/thumbs
 cp /var/WaveOS/modeless/features/multistep-menu/images/thumbs/2.jpg /var/www/html/images/thumbs
