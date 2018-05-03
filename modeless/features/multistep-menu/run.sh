39f48925233d6688a19460fab08e0c4338d10982
unclehowell <hywelapbuckler@gmail.com>
2018-05-03 14:04:50 -0300

    user permission bug
    stopped before webpage installed.
    may have to do with removing tomcat install. But keeping tomcat removed. will find a different resolution

--- modeless/features/multistep-menu/run.sh
+++ modeless/features/multistep-menu/run.sh
@@ -1,27 +1,27 @@
-sudo rm -r /var/www/html/index.html
+rm -r /var/www/html/index.html
 
-cp /var/WaveOS/modeless/features/multistep-menu/index.html /var/www/html
+mv /var/WaveOS/modeless/features/multistep-menu/index.html /var/www/html
 
 mkdir /var/www/html/js
 
-cp /var/WaveOS/modeless/features/multistep-menu/js/modernizr.custom.js /var/www/html/js
-cp /var/WaveOS/modeless/features/multistep-menu/js/grid.js /var/www/html/js
+mv /var/WaveOS/modeless/features/multistep-menu/js/modernizr.custom.js /var/www/html/js
+mv /var/WaveOS/modeless/features/multistep-menu/js/grid.js /var/www/html/js
 
 mkdir /var/www/html/css
 
-cp /var/WaveOS/modeless/features/multistep-menu/css/component.css /var/www/html/css
-cp /var/WaveOS/modeless/features/multistep-menu/css/default.css /var/www/html/css
+mv /var/WaveOS/modeless/features/multistep-menu/css/component.css /var/www/html/css
+mv /var/WaveOS/modeless/features/multistep-menu/css/default.css /var/www/html/css
 
 mkdir /var/www/html/images
 
-cp /var/WaveOS/modeless/features/multistep-menu/images/1.jpg /var/www/html/images
-cp /var/WaveOS/modeless/features/multistep-menu/images/2.jpg /var/www/html/images
-cp /var/WaveOS/modeless/features/multistep-menu/images/3.png /var/www/html/images
+mv /var/WaveOS/modeless/features/multistep-menu/images/1.jpg /var/www/html/images
+mv /var/WaveOS/modeless/features/multistep-menu/images/2.jpg /var/www/html/images
+mv /var/WaveOS/modeless/features/multistep-menu/images/3.png /var/www/html/images
 
 mkdir /var/www/html/images/thumbs
 
-cp /var/WaveOS/modeless/features/multistep-menu/images/thumbs/1.jpg /var/www/html/images/thumbs
-cp /var/WaveOS/modeless/features/multistep-menu/images/thumbs/2.jpg /var/www/html/images/thumbs
-cp /var/WaveOS/modeless/features/multistep-menu/images/thumbs/3.png /var/www/html/images/thumbs
+mv /var/WaveOS/modeless/features/multistep-menu/images/thumbs/1.jpg /var/www/html/images/thumbs
+mv /var/WaveOS/modeless/features/multistep-menu/images/thumbs/2.jpg /var/www/html/images/thumbs
+mv /var/WaveOS/modeless/features/multistep-menu/images/thumbs/3.png /var/www/html/images/thumbs
 
 cd
