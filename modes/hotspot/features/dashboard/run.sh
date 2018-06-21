9bd90163a0fb940e8c56fdeb03ef842f9847c052
Hywel ApBuckler <10119070+unclehowell@users.noreply.github.com>
2018-06-21 21:33:49 +0100

    Update run.sh

--- modes/hotspot/features/dashboard/run.sh
+++ modes/hotspot/features/dashboard/run.sh
@@ -9,6 +9,7 @@ mv /var/www/html/makeitwave.com-master/docs/demo.html /var/www/html
 mkdir /var/www/html/dashboard-demo
 mv /var/www/html/makeitwave.com-master/docs/dashboard-demo/* /var/www/html/dashboard-demo
 mv /var/www/html/demo.html /var/www/html/index.html
+rm -r /var/www/html/makeitwave.com-master
 
 # more to do, including fix missing bits, rename links to go to apps, not demo pages etc - but this'll do for now pigs
 
