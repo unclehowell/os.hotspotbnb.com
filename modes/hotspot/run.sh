a9729caa52fe68647786584fed69906d7e994332
Hywel ApBuckler <10119070+unclehowell@users.noreply.github.com>
2018-06-23 14:40:38 +0100

    Update run.sh

--- modes/hotspot/run.sh
+++ modes/hotspot/run.sh
@@ -2,5 +2,22 @@
 
 # install hotspot-mode: features
 
-# pihole
+# dashboard
+sh /var/WaveOS/modes/hotspot/features/dashboard/run.sh
+
+# app installing page - places an 'app installing page' in the dashboard frame until the apps installed
+
+# pihole - faster internet
 sh /var/WaveOS/modes/hotspot/features/pihole/run.sh
+
+# emby - media & tv
+
+# motioneye - ipcctv
+
+# openhab - smart home
+
+# emoncms - energy monitoring
+
+# net stats
+
+
