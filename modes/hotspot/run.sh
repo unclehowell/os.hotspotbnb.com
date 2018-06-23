a6b6d1b47772b98852428066f1d02c446a2ca79f
Hywel ApBuckler <10119070+unclehowell@users.noreply.github.com>
2018-06-23 15:30:56 +0100

    Update run.sh

--- modes/hotspot/run.sh
+++ modes/hotspot/run.sh
@@ -11,13 +11,18 @@ sh /var/WaveOS/modes/hotspot/features/dashboard/run.sh
 sh /var/WaveOS/modes/hotspot/features/pihole/run.sh
 
 # emby - media & tv
+sh /var/WaveOS/modes/hotspot/features/emby/run.sh
 
 # motioneye - ipcctv
+sh /var/WaveOS/modes/hotspot/features/motioneye/run.sh
 
 # openhab - smart home
+sh /var/WaveOS/modes/hotspot/features/openhab/run.sh
 
 # emoncms - energy monitoring
+sh /var/WaveOS/modes/hotspot/features/emoncms/run.sh
 
 # net stats
+sh /var/WaveOS/modes/hotspot/features/netstatsrun.sh
 
 
