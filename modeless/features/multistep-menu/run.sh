825a31c8243984285dea50f9975e1b9604917689
Hywel ApBuckler <hywelapbuckler@gmail.com>
2018-06-24 17:19:52 -0400

    updates

--- modeless/features/multistep-menu/run.sh
+++ modeless/features/multistep-menu/run.sh
@@ -1,27 +1,22 @@
+# clear the content of the web-server directory
 rm -r /var/www/html/index.html
+rm -r /var/www/html/img
+rm -r /var/www/html/images
+rm -r /var/www/html/js
+rm -r /var/www/html/css
+rm -r /var/www/html/cgi-redirectors
 
-mv /var/WaveOS/modeless/features/multistep-menu/index.html /var/www/html
-
+# place the mode select menu into the web-server directory
 mkdir /var/www/html/js
-
-mv /var/WaveOS/modeless/features/multistep-menu/js/modernizr.custom.js /var/www/html/js
-mv /var/WaveOS/modeless/features/multistep-menu/js/grid.js /var/www/html/js
-
 mkdir /var/www/html/css
-
-mv /var/WaveOS/modeless/features/multistep-menu/css/component.css /var/www/html/css
-mv /var/WaveOS/modeless/features/multistep-menu/css/default.css /var/www/html/css
-
 mkdir /var/www/html/images
-
-mv /var/WaveOS/modeless/features/multistep-menu/images/1.jpg /var/www/html/images
-mv /var/WaveOS/modeless/features/multistep-menu/images/2.jpg /var/www/html/images
-mv /var/WaveOS/modeless/features/multistep-menu/images/3.png /var/www/html/images
-
 mkdir /var/www/html/images/thumbs
+mkdir /var/www/html/cgi-redirectors
 
-mv /var/WaveOS/modeless/features/multistep-menu/images/thumbs/1.jpg /var/www/html/images/thumbs
-mv /var/WaveOS/modeless/features/multistep-menu/images/thumbs/2.jpg /var/www/html/images/thumbs
-mv /var/WaveOS/modeless/features/multistep-menu/images/thumbs/3.png /var/www/html/images/thumbs
-
-cd
+# move the content to the right directories
+mv /var/WaveOS/modeless/features/multistep-menu/index.html /var/www/html
+mv /var/WaveOS/modeless/features/multistep-menu/js/* /var/www/html/js
+mv /var/WaveOS/modeless/features/multistep-menu/css/* /var/www/html/css
+mv /var/WaveOS/modeless/features/multistep-menu/images/* /var/www/html/images
+mv /var/WaveOS/modeless/features/multistep-menu/images/thumbs/* /var/www/html/images/thumbs
+mv /var/WaveOS/modeless/features/multistep-menu/cgi-redirectors/* /var/www/html/cgi-redirectors
