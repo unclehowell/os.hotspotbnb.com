38cc57f05c274f4fc311b1ced6ddf6074bfc5326
Hywel ApBuckler <hywelapbuckler@gmail.com>
2018-06-24 22:20:18 -0400

    update solution
    self-erasing the update script file content (not the file) was key to success, but a bugger. But I figured it. Now it works. Woo Hoo

--- updates/current/00-04-01.sh
+++ updates/current/00-04-01.sh
@@ -2,6 +2,10 @@
 
 # update the brand logo
 
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+printf "${RED}WAVEOS-version-0-4-1${NC} - The system will now update from version 0.4.0 to version 0.4.1\n"
+
 cd /var/www/html/images/thumbs/
 rm -r 3.png
 wget http://makeitwave.com/img/wave_logo.png
@@ -24,9 +28,22 @@ sed -i 's/0.4.0/0.4.1/g' version.txt
 cd
 
 # should run cleaner on every update, for benefit of firmware updates and general housekeeping
-sh /var/WaveOS/modeless/features/cleanup.sh
+sh /var/WaveOS/modeless/features/cleanup.sh ;
 
 
 # erase content of this file to ensure run once
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+printf "${RED}WaveOS is now at version 0.4.1${NC} - Self-destructing this update script so it wont run again and create a loop\n" ;
 
-cp /dev/null 00-04-01.sh
+cd
+
+# This is how we erase the content of this file
+cd /usr/lib/WaveOS-updates
+printf 'sleep 30\n sudo rm -r /usr/lib/WaveOS-updates/00-04-01.sh\n sudo touch /usr/lib/WaveOS-updates/00-04-01.sh\n' > 00-04-01-01.sh
+chmod +x 00-04-01-01.sh
+sudo sh /usr/lib/WaveOS-updates/00-04-01-01.sh
+cd
+exit 0
+ 
+ 
