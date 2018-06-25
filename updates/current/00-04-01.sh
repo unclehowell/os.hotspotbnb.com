1a44360367498d676d2967f8927376f078216e1b
Hywel ApBuckler <hywelapbuckler@gmail.com>
2018-06-25 02:05:20 -0400

    few more tweaks

--- updates/current/00-04-01.sh
+++ updates/current/00-04-01.sh
@@ -4,7 +4,7 @@
 
 RED='\033[0;31m'
 NC='\033[0m' # No Color
-printf "${RED}WAVEOS-version-0-4-1${NC} - The system will now update from version 0.4.0 to version 0.4.1\n"
+printf "\n\n${RED}WAVEOS-version-0-4-1${NC} - The system will now update from version 0.4.0 to version 0.4.1\n\n"
 
 # added this to the hosting.sh - it's used for the new update process 
 sudo apt-get install subversion -y
@@ -21,20 +21,17 @@ cd /var/www/html/
 sed -i 's/0.4.0/0.4.1/g' version.txt
 cd
 
-# should run cleaner on every update, for benefit of firmware updates and general housekeeping
-sh /var/WaveOS/modeless/features/cleanup.sh ;
-
 
 # erase content of this file to ensure run once
 RED='\033[0;31m'
 NC='\033[0m' # No Color
-printf "${RED}WaveOS is now at version 0.4.1${NC} - Self-destructing this update script so it wont run again and create a loop\n" ;
+printf "\n\n${RED}WaveOS is now at version 0.4.1${NC} - Self-destructing this update script so it wont run again and create a loop\n\n" ;
 
 cd
 
 # This is how we erase the content of this file
 cd /usr/lib/WaveOS-updates
-printf 'sleep 30\n sudo rm -r /usr/lib/WaveOS-updates/00-04-01.sh\n sudo touch /usr/lib/WaveOS-updates/00-04-01.sh\n' > 00-04-01-01.sh
+printf 'sleep 5\n sudo rm -r /usr/lib/WaveOS-updates/00-04-01.sh\n sudo touch /usr/lib/WaveOS-updates/00-04-01.sh\n' > 00-04-01-01.sh
 chmod +x 00-04-01-01.sh
 sudo sh /usr/lib/WaveOS-updates/00-04-01-01.sh
 cd
