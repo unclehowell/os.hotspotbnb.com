22529f9825dcb32b5f5e9e72d759e3b15c9920ab
WaveOS <hywelapbuckler@gmail.com>
2018-06-29 18:42:47 -0400

    Update 00-04-02.sh

--- updates/current/00-04-02.sh
+++ updates/current/00-04-02.sh
@@ -9,10 +9,6 @@ printf "\n\n${RED}WAVEOS-version-0-4-1${NC} - The system will now update from ve
 cd /var/www/html/
 sed -i 's/0.4.1/0.4.2/g' version.txt
 
-# moving the location of the version number, because the html folder is wiped when the dashboard is installed, so not a good place to keep it
-cp version.txt /var/www/
-cd
-
 # fix the cgi issue within apache (for mode-select to function)
 
 # 1. Hosting.sh updated to fix all the cgi issues, so we just grab the latest WaveOS repo and re-run that
