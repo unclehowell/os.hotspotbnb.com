fcb133571d6c097afd762dac13de469e06dd4df8
WaveOS <hywelapbuckler@gmail.com>
2018-06-30 19:53:50 +0100

    Update 00-04-02.sh

--- updates/current/00-04-02.sh
+++ updates/current/00-04-02.sh
@@ -22,4 +22,18 @@ sh /var/WaveOS/modeless/features/hosting.sh
 
 
 
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+printf "\n\n${RED}WaveOS is now at version 0.4.2${NC}\n\n" ;
+
+cd
+
+# Then we systematically erase the content of this file to avoid it running again. 
+cd /usr/lib/WaveOS-updates
+printf 'sleep 5\n sudo rm -r /usr/lib/WaveOS-updates/00-04-02.sh\n sudo touch /usr/lib/WaveOS-updates/00-04-02.sh\n' > 00-04-02-01.sh
+chmod +x 00-04-02-01.sh
+sudo sh /usr/lib/WaveOS-updates/00-04-02-01.sh
+cd
+
+exit 0
 
