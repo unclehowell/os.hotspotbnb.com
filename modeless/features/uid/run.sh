67b0136df24188badbec566b2686cff280b1be0f
WaveOS <hywelapbuckler@gmail.com>
2018-06-30 20:04:01 -0400

    Update run.sh

--- modeless/features/uid/run.sh
+++ modeless/features/uid/run.sh
@@ -4,4 +4,6 @@ cd /var/www/html
 sed -i 's/Serial//g' uid.txt
 sed -i 's/://g' uid.txt
 sed -i 's/[[:space:]]//g' uid.txt
+sed -i 's/[[:blank:]]//g' uid.txt
+sed -i 's/\<00*\([1-9]\)/\1/g' uid.txt
 cd
