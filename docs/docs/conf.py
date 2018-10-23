a0b3a204a718d840f8693470230df5ce43b2cde7
WaveOS <hywelapbuckler@gmail.com>
2018-10-23 16:35:41 -0400

    update readthedocs template

--- docs/docs/conf.py
+++ docs/docs/conf.py
@@ -45,7 +45,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'tutos'
-copyright = '2014, kotrfa'
+copyright = '2014, Wave'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -198,7 +198,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
   ('index', 'tutos.tex', 'tutos Documentation',
-   'kotrfa', 'manual'),
+   'Wave', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -228,7 +228,7 @@ latex_documents = [
 # (source start file, name, description, authors, manual section).
 man_pages = [
     ('index', 'tutos', 'tutos Documentation',
-     ['kotrfa'], 1)
+     ['Wave'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -242,7 +242,7 @@ man_pages = [
 #  dir menu entry, description, category)
 texinfo_documents = [
   ('index', 'tutos', 'tutos Documentation',
-   'kotrfa', 'tutos', 'One line description of project.',
+   'Wave', 'tutos', 'One line description of project.',
    'Miscellaneous'),
 ]
 
